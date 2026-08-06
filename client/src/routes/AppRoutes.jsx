545f283e027c967a79d2292d1bdfb57a8239e480
AnuHewage <anuruddhikadulanjanee@gmail.com>
2026-08-06 14:34:23 +0530

    refactor: organize help support pages into feature structure

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+
 import FoundationPage from '../pages/FoundationPage.jsx';
 import NotFoundPage from '../pages/NotFoundPage.jsx';
-import AboutPage from '../pages/AboutPage.jsx';
-import ContactPage from '../pages/ContactPage.jsx';
-import FAQPage from '../pages/FAQPage.jsx';
-import HelpPage from '../pages/HelpPage.jsx';
-import SupportFormPage from '../pages/SupportFormPage.jsx';
+
+import AboutPage from '../features/help-support/pages/AboutPage.jsx';
+import ContactPage from '../features/help-support/pages/ContactPage.jsx';
+import FAQPage from '../features/help-support/pages/FAQPage.jsx';
+import HelpPage from '../features/help-support/pages/HelpPage.jsx';
+import SupportFormPage from '../features/help-support/pages/SupportFormPage.jsx';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<FoundationPage />} />
+
       <Route path="/about" element={<AboutPage />} />
       <Route path="/contact" element={<ContactPage />} />
       <Route path="/faq" element={<FAQPage />} />
