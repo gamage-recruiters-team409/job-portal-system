192ea1cdb25c574655cd1483711222f756c1ff04
AnuHewage <anuruddhikadulanjanee@gmail.com>
2026-08-07 16:53:57 +0530

    fix: improve support controller error handling and response security

--- server/src/controllers/support.controller.js
+++ server/src/controllers/support.controller.js
@@ -3,26 +3,18 @@ import {
 } from "../services/support.service.js";
 
 
-export const createSupportMessage = async (req, res) => {
+export const createSupportMessage = async (req, res, next) => {
   try {
 
-    const supportMessage = await createSupport(req.body);
+    await createSupport(req.body);
 
     res.status(201).json({
       success: true,
       message: "Support message submitted successfully",
-      data: supportMessage,
     });
 
   } catch (error) {
-
-    console.error("Support Controller Error:", error);
-
-    res.status(500).json({
-      success: false,
-      message: "Internal server error",
-    });
-
+    next(error);
   }
 };
 
