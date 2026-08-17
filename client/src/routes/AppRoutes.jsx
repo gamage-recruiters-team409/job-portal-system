076a04634ae9b02ff2fb7fa5fe480d4d3935ba62
hiba-J165 <hibajailabdeen@gmail.com>
2026-08-17 12:06:16 +0530

    feat(profile): add experience management UI

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -33,6 +33,7 @@ import EditProfilePage from '../features/job-seeker-profile/pages/EditProfilePag
 import ProfileCompletionPage from '../features/job-seeker-profile/pages/ProfileCompletionPage.jsx';
 import SkillsPage from '../features/job-seeker-profile/pages/SkillsPage.jsx';
 import EducationPage from '../features/job-seeker-profile/pages/EducationPage.jsx';
+import ExperiencePage from '../features/job-seeker-profile/pages/ExperiencePage.jsx';
 
 function AppRoutes() {
   return (
@@ -233,6 +234,17 @@ function AppRoutes() {
           <ProtectedRoute allowedRoles={[USER_ROLES.JOB_SEEKER]}>
             <AuthenticatedLayout>
               <EducationPage />
+            </AuthenticatedLayout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profile/experience"
+        element={
+          <ProtectedRoute allowedRoles={[USER_ROLES.JOB_SEEKER]}>
+            <AuthenticatedLayout>
+              <ExperiencePage />
             </AuthenticatedLayout>
           </ProtectedRoute>
         }
