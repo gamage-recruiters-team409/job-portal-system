cd1aeafa05683880094fb96899fe09e41735af79
AnuHewage <anuruddhikadulanjanee@gmail.com>
2026-08-20 09:46:48 +0530

    Fix duplicate job report validation

--- server/src/services/report.service.js
+++ server/src/services/report.service.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import Job from '../models/Job.js';
 import Report from '../models/Report.js';
 import { ApiError } from '../utils/apiError.js';
-import { JOB_STATUSES } from '../constants/statuses.js';
+import { JOB_STATUSES, REPORT_STATUSES } from '../constants/statuses.js';
 
 export const createReport = async (reportData) => {
   const { jobId, reason, description, reportedBy } = reportData;
@@ -20,6 +20,24 @@ export const createReport = async (reportData) => {
   if (!job) {
     throw new ApiError(404, 'Job not found.');
   }
+
+  const existingReport = await Report.findOne({
+  jobId: job._id,
+  reportedBy,
+  status: {
+    $in: [
+      REPORT_STATUSES.PENDING,
+      REPORT_STATUSES.UNDER_REVIEW,
+    ],
+  },
+});
+
+if (existingReport) {
+  throw new ApiError(
+    400,
+    'You have already reported this job. Please wait until the previous report is reviewed.'
+  );
+}
 
   const report = await Report.create({
     jobId: job._id,
